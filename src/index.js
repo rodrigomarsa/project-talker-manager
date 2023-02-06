3f6aa49bc53048ca59f4d679051e29153235781f
Rodrigo <rodrigomartins.agro@gmail.com>
2023-02-06 15:25:39 -0300

    feat: add requisito 01

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talkersPath = path.resolve(__dirname, './talker.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -17,4 +18,20 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
-module.exports = app;
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talkersPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const talkers = await readFile();
+    return res.status(200).json(talkers);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
